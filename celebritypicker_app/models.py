a6815c679e4fd194a5196104e4e89dd5c07f8681
JaCrawford92 <cckenny9@gmail.com>
2024-05-14 14:44:51 -0500

    models added

--- celebritypicker_app/models.py
+++ celebritypicker_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class profile(models.Model):
+    date = models.DateField()
+    movie = models.CharField(max_length=50)
+    show = models.CharField(max_length=50) 
+
+class actor(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+class movie(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.DateField()
+    actors = models.ManyToManyField(actor)
+
+    def __str__(self):
+        return self.title
+    
+class show(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.DateField()
+    actors = models.ManyToManyField(actor)
+
+    def __str__(self):
+        return self.title
