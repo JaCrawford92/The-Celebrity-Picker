7731527fa191485f56ecb3df588a25e817fc8980
JaCrawford92 <cckenny9@gmail.com>
2024-05-14 19:11:36 -0500

    home page is rendering

--- celebritypicker_app/views.py
+++ celebritypicker_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, '/home.html')
+    return render(request, 'celebritypicker/home.html')
 
 def signup (request):
     return render(request, '/signup.html')
